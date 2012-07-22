9132646ef5f8cdae9c54e714ce899510f3cea8ae
Cory Mawhorter <cory.mawhorter@ephective.com>
2012-07-21 21:17:33 -0700

    removed log

--- nntp.js
+++ nntp.js
@@ -91,7 +91,6 @@ NNTP.prototype.connect = function(port, host) {
         // new response
         code = parseInt(curData.substring(idxStart, 3), 10);
         text = curData.substring(3, idxCRLF).trim();
-        console.log(self._queue);
         if (isML = (respsML.indexOf(code) > -1
                     || (code === 211 && self._queue[0][0] === 'LISTGROUP')))
           self._MLEmitter = new EventEmitter();
